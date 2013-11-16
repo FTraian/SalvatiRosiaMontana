2cac55f1aaa48ae0e4d1a2a689567c918103d095
Traian Fratean <traian.fratean@gmail.com>
2013-11-17 00:16:50 +0200

    Initial commit

---
+++ src/ro/iss/salvatirosiamontana/Settings.java
@@ -0,0 +1,83 @@
+package ro.iss.salvatirosiamontana;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener {
+	@SuppressWarnings("deprecation")
+	ListPreference languageList;
+	ListPreference cityList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+		languageList = (ListPreference) findPreference("language");
+		cityList = (ListPreference) findPreference("city");
+		languageList
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						final String val = newValue.toString();
+						int index = languageList.findIndexOfValue(val);
+						languageList.setSummary(val);
+						if (index == 0) {
+							cityList.setEnabled(true);
+						} else {
+							cityList.setEnabled(false);
+						}
+						return true;
+					}
+				});
+
+		cityList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+			public boolean onPreferenceChange(Preference preference,
+					Object newValue) {
+				final String val = newValue.toString();
+				cityList.setSummary(val);
+				
+				
+				return true;
+			}
+		});
+
+		ListPreference listPreference = (ListPreference) findPreference("language");
+		if (listPreference.getValue() == null) {
+			// to ensure we don't get a null value
+			// set first value by default
+			listPreference.setValueIndex(0);
+		} else {
+			if ("International".equalsIgnoreCase(listPreference.getValue())) {
+				cityList.setEnabled(false);
+			}
+		}
+		listPreference.setSummary(listPreference.getEntry().toString());
+		listPreference = (ListPreference) findPreference("city");
+		if (listPreference.getValue() == null) {
+			// to ensure we don't get a null value
+			// set first value by default
+			listPreference.setValueIndex(0);
+		}
+		listPreference.setSummary(listPreference.getEntry().toString());
+
+	}
+
+	
+	
+
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		Preference pref = findPreference(key);
+
+		if (pref instanceof ListPreference) {
+			ListPreference listPref = (ListPreference) pref;
+			pref.setSummary(listPref.getEntry());
+		}
+	}
+
+}
